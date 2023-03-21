65b6724d8e3825743ded6607fdd9f27cd4812c9f
Jose Guilherme Rocha Vaz Francisco <jrocha-v@c1r4s2.42porto.com>
2023-03-21 12:20:26 +0000

    c04-atoi

--- c04/ex03/ft_atoi.c
+++ c04/ex03/ft_atoi.c
@@ -6,46 +6,36 @@
 /*   By: jrocha-v <jrocha-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 19:10:59 by jrocha-v          #+#    #+#             */
-/*   Updated: 2023/03/21 11:24:59 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2023/03/21 12:19:21 by jrocha-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
-
 int	ft_atoi(char *str)
 {
 	int i;
-	int x;
 	int ret;
+	int	sign;
 	
 	i = 0;
-	x = 1;
+	sign = 1;
 	ret = 0;
-	while (str[i] != '\0')
-	{
-		if (str[++i] == '-')
-			x = x * -1;
-		else if (str[++i] == '+')
-			x = x * 1;
-		else if (str[i] >= '0' && str[i] <= '9')
-		{
-			while (str[i] >= '0' && str[i] <= '9')
-			{
-				ret = (ret * 10) + str[i] - 48;
-				i++;		
-			}
-			return (ret * x);
-		}
-		else
-			i++;
-	}
-	return (ret);
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+		if (str[i++] == '-')
+			sign *= -1;
+	while (str[i] >= '0' && str[i] <= '9')
+		ret = ret * 10 + str[i++] - '0';
+	return (ret * sign);
 }
 
-int	main(int argc, char **argv)
+/*int	main(int argc, char **argv)
 {
 	if (argc == 2)
 		printf("%i", ft_atoi(argv[1]));
-}
+	else
+		return (0);
+}*/
