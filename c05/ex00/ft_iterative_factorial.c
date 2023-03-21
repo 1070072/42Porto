747e0599ae7e4e78698492cfeab8fb0b8d4e926b
José Francisco <jf@MacBook-Pro-de-Jose-2.local>
2023-03-21 19:15:31 +0000

    c05_

--- c05/ex00/ft_iterative_factorial.c
+++ c05/ex00/ft_iterative_factorial.c
@@ -3,10 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jrocha-v <jrocha-v@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jf <jf@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 18:35:19 by jrocha-v          #+#    #+#             */
-/*   Updated: 2023/03/21 18:35:19 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2023/03/21 18:52:21 by jf               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include <stdio.h>
+
+int ft_iterative_factorial(int nb)
+{
+	int f;
+
+	f = 1;
+	while (nb > 0)
+	{
+		f = f * nb;
+		nb--;
+	}
+	return (f);	
+}
+
+int	main(void)
+{
+	int n;
+	int res;
+
+	n = -5;
+	while (n <= 10)
+	{
+		res = ft_iterative_factorial(n);
+		printf("n = %d, n! = %d\n", n, res);
+		n++;
+	}
+	return (0);
+}
--- c05/ex01/ft_recursive_factorial.c
+++ c05/ex01/ft_recursive_factorial.c
@@ -3,10 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jrocha-v <jrocha-v@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jf <jf@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 18:35:55 by jrocha-v          #+#    #+#             */
-/*   Updated: 2023/03/21 18:35:55 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2023/03/21 19:10:17 by jf               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include <stdio.h>
+
+int	ft_recursive_factorial(int nb)
+{
+	int f;
+
+	if (nb <= 0)
+	return (0);
+
+	while (nb > 0)
+	{
+		f = f * nb;
+		ft_recursive_factorial(f);	
+	}
+	return(f);
+}
