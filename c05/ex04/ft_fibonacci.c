8dc6fd3bd5b1f193e73a59ae34583ddaf99f1a6e
José Francisco <jf@MacBook-Pro-de-Jose-2.local>
2023-03-22 11:54:37 +0000

    fibo

--- c05/ex04/ft_fibonacci.c
+++ c05/ex04/ft_fibonacci.c
@@ -3,22 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   ft_fibonacci.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jrocha-v <jrocha-v@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jf <jf@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 18:37:22 by jrocha-v          #+#    #+#             */
-/*   Updated: 2023/03/22 10:40:40 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2023/03/22 11:52:25 by jf               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
 
 int	ft_fibonacci(int index)
 {
 	if (index < 0)
 		return (-1);
+	else if (index < 2)
+		return (index);
 	else
-		return(index + ft_fibonacci(index - 1));
+		return(ft_fibonacci(index - 2) + ft_fibonacci(index - 1));
 }
 
 int	main(void)
 {
-	printf()
+	int res;
+	int index;
+
+	index = 0;
+	while (index < 12)
+	{
+		res = ft_fibonacci(index);
+		printf ("%d, ", res);
+		index++;
+	}
+	res = ft_fibonacci(index);
+	printf ("%d\n", res);
+	return (0);
 }
