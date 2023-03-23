2b75203219ec8434a3c4d95124690336c607b304
Jose Guilherme Rocha Vaz Francisco <jrocha-v@c1r4s1.42porto.com>
2023-03-23 14:29:38 +0000

    teste

--- c07/ex01/ft_range.c
+++ c07/ex01/ft_range.c
@@ -6,27 +6,40 @@
 /*   By: jrocha-v <jrocha-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 08:34:01 by jrocha-v          #+#    #+#             */
-/*   Updated: 2023/03/23 11:55:16 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2023/03/23 14:29:05 by jrocha-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include <stdio.h>
+
 int	*ft_range(int min, int max)
 {
+	int size;
+	int	*range;
+	int i;
 	
-
-	
+	i = -1;
+	size = max - min;
+	if (min >= max)
+	return (NULL);
+	range = (int*)malloc(size * 4);
+	while (++i < size)
+		range[i] = min + i;	
+	return (range);
 }
 
 int	main(void)
 {
 	int min;
 	int max;
-
+	int *r;
+	int i;
+		
 	min = -10;
 	max = 10;
 	
-	ft_range(min, max);
-	
-	
-	
+	r = ft_range(min, max);	
+	while (
+	printf("%d\n", *r);
 }
