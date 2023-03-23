7a210b10086570c5f91c4a9b90909aa733babb63
José Francisco <jf@MacBook-Pro-de-Jose-2.local>
2023-03-23 19:46:50 +0000

    ex

--- EXMS/exm01/2-rot13/_oldrot13.c
+++ EXMS/exm01/2-rot13/_oldrot13.c
@@ -1,37 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   first_word.c                                       :+:      :+:    :+:   */
+/*   rot13.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jf <jf@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/16 21:28:35 by jf                #+#    #+#             */
-/*   Updated: 2023/03/23 19:22:27 by jf               ###   ########.fr       */
+/*   Created: 2023/03/16 21:50:49 by jf                #+#    #+#             */
+/*   Updated: 2023/03/16 22:25:17 by jf               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+/*
+char ft_rotate(char **argv[1][i])
+{
+	if (('A' <= argv[1][i] && 'Z' >= argv[1][i]) || ('A' <= argv[1][i] && 'Z' >= argv[1][i]))
+	{
+		argv[1][i] = argv[1][i] + 13 - 25;		
+	}
+	return(**argv[1][i]);
+}*/
 
 int	main(int argc, char **argv)
 {
 	int i;
 	
 	i = 0;
-	if (argc < 2 || argc > 2)
-	write(1, "\n", 1);
-
-	while ((argv[1][i] >= 9 && argv[1][i] <= 13) || argv[1][i] == 32)
-	i++;
-	if ((argv[1][i] >= 'a' && argv[1][i] <= 'z') || (argv[1][i] >= 'A' && argv[1][i] <= 'Z'))
-	{	
-		while (!((argv[1][i] >= 9 && argv[1][i] <= 13) || argv[1][i] == 32))
+	if (argc == 2)
+	{
+		while (argv[1][i] != '\0')
 		{
+			if (('a' <= argv[1][i] && 'm' >= argv[1][i]) || ('A' <= argv[1][i] && 'M' >= argv[1][i]))
+				argv[1][i] = argv[1][i] + 13;
+			else if (('n' <= argv[1][i] && 'z' >= argv[1][i]) || ('N' <= argv[1][i] && 'Z' >= argv[1][i]))
+				argv[1][i] = argv[1][i] - 13;
 			write(1, &argv[1][i], 1);
 			i++;
-		}
+		}		
 	}
-	else
-	write(1, "\n", 1);
+	write (1, "\n", 1);
+	return (0);
 }
--- EXMS/exm01/2-rot13/rot13.c
+++ EXMS/exm01/2-rot13/rot13.c
@@ -5,41 +5,37 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jf <jf@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/16 21:50:49 by jf                #+#    #+#             */
-/*   Updated: 2023/03/16 22:25:17 by jf               ###   ########.fr       */
+/*   Created: 2023/03/23 19:30:39 by jf                #+#    #+#             */
+/*   Updated: 2023/03/23 19:46:23 by jf               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-/*
-char ft_rotate(char **argv[1][i])
-{
-	if (('A' <= argv[1][i] && 'Z' >= argv[1][i]) || ('A' <= argv[1][i] && 'Z' >= argv[1][i]))
-	{
-		argv[1][i] = argv[1][i] + 13 - 25;		
-	}
-	return(**argv[1][i]);
-}*/
 
 int	main(int argc, char **argv)
 {
 	int i;
 	
 	i = 0;
+	
 	if (argc == 2)
 	{
 		while (argv[1][i] != '\0')
 		{
-			if (('a' <= argv[1][i] && 'm' >= argv[1][i]) || ('A' <= argv[1][i] && 'M' >= argv[1][i]))
+			if (('a' <= argv[1][i] && 'm' >= argv[1][i]) || ('A' <= argv[1][i] && 'M' >= argv[1][i])) 
+			{	
 				argv[1][i] = argv[1][i] + 13;
-			else if (('n' <= argv[1][i] && 'z' >= argv[1][i]) || ('N' <= argv[1][i] && 'Z' >= argv[1][i]))
+				write(1, &argv[1][i], 1);
+			}	
+			else if (('n' <= argv[1][i] && 'z' >= argv[1][i]) || ('N' <= argv[1][i] && 'Z' >= argv[1][i]))  
+			{	
 				argv[1][i] = argv[1][i] - 13;
+				write(1, &argv[1][i], 1);
+			}
+			else
 			write(1, &argv[1][i], 1);
-			i++;
-		}		
+			i++;		
+		}
 	}
-	write (1, "\n", 1);
-	return (0);
+	write(1, "\n", 1);
 }
