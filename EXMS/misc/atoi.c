67282c5e7f9e49bfe1fbb1f9551725e07555e698
Jose Guilherme Rocha Vaz Francisco <jrocha-v@c1r5s4.42porto.com>
2023-03-24 09:55:19 +0000

    s

--- EXMS/misc/atoi.c
+++ EXMS/misc/atoi.c
@@ -1,60 +1,60 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   atoi.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jrocha-v <jrocha-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/23 17:41:46 by jf                #+#    #+#             */
-/*   Updated: 2023/03/24 08:26:26 by jrocha-v         ###   ########.fr       */
+/*   Created: 2023/03/24 08:13:22 by jrocha-v          #+#    #+#             */
+/*   Updated: 2023/03/24 09:06:24 by jrocha-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
 
-int	ft_atoi(const char *str)
+int	ft_atoi(char *str)
 {
 	int i;
-	int s;
+	int c;
 	int sign;
 	int res;
-			
+	
 	i = -1;
-	s = 0;
+	c = 0;
 	sign = 1;
 	res = 0;
 	while (str[++i] != '\0')
-	{	
+	{
 		if (str[i] == '-')
 		{
 			sign = sign * -1;
-			s++;
-		}
-		else if (str[i] == '+')
-			s++;
-		else
-			sign = sign * 1;
+			c++;
+		}		 
+		if (str[i] == '+')
+			c++;
+		if (c > 2)
+			return (0);		
 	}
-	if (s > 1)
-		return (0);
 	i = 0;
-	while (str[i] >= 9 && str[i] <= 13 || str[i] == 32)
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
 		i++;
 	while (str[i] == '-' || str[i] == '+')
 		i++;
-	while (str[i] >= '0' && str[i] <= '9')
+	while(str[i] >= '0' && str[i] <= '9')
 		res = res * 10 + str[i++] - '0';
-	return (res * sign);		
-}		
+	return (res * sign);
+}
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	int at = 0;
-	char str[] = " d +-1234fdsfsdf";
-	
-	at = atoi(str);
-	printf("%i\n", at);
-	at = ft_atoi(str);
-	printf("%i\n", at);
+	int nb;
+
+	nb = 0;
+	if (argc == 2)
+	{
+		nb = ft_atoi(argv[1]);
+		printf("%i", nb);
+		printf("\n");
+	}
 }
