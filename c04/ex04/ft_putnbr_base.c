ca8f0c2c57d6fbb6dfa38383661c0858f6cfdb6b
Jose Guilherme Rocha Vaz Francisco <jrocha-v@c1r7s3.42porto.com>
2023-03-27 12:42:43 +0100

    updt

--- c04/ex04/ft_putnbr_base.c
+++ c04/ex04/ft_putnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: jrocha-v <jrocha-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 17:01:10 by jrocha-v          #+#    #+#             */
-/*   Updated: 2023/03/21 18:15:57 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2023/03/27 12:40:27 by jrocha-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,21 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-int	ft_strlen(char *base)
+int ft_ch_db(char *base, char b, int pos)
+{
+	int k;
+
+	k = 0;
+	while(k < pos)
+	{
+		if(base[k] == b)
+			return (0);
+		k++;
+	}	
+	return (1);	
+}
+
+int	ft_sl(char *base)
 {
 	int	i;
 
@@ -37,8 +51,8 @@ int	ft_check_base(char *base)
 	while (base[++i] != '\0')
 	{
 		if (base[i] == '\0' || base[i] == '+'
-			|| base[i] == '-' || base[i] == base [i + 1]
-			|| ft_strlen(base) <= 1)
+			|| base[i] == '-' || (ft_ch_db(base, base[i], i)) == 0
+			|| ft_sl(base) <= 1)
 			return (0);
 		else
 			return (1);
@@ -52,7 +66,7 @@ void	ft_putnbr_base(int nbr, char *base)
 	unsigned int	n;
 	unsigned int	j;
 
-	j = ft_strlen(base);
+	j = ft_sl(base);
 	if (ft_check_base(base) == 0)
 		return ;
 	if (nbr < 0)
@@ -68,9 +82,9 @@ void	ft_putnbr_base(int nbr, char *base)
 	write(1, &l, 1);
 }		
 
-/*int		main(void)
+int		main(void)
 {
-	// base binaria
+	/*// base binaria
 	printf("Bases binarias, n = 47. \"01\" e \"\\/\"\n");
 	ft_putnbr_base(47, "01");
 	ft_putchar('\n');
@@ -85,18 +99,18 @@ void	ft_putnbr_base(int nbr, char *base)
 	printf("Esperado: -2147483648\n");
 	ft_putnbr_base(INT_MIN, ",.;\\][{}@#");
 	ft_putchar('\n');
-	printf("Esperado: -;.]}]@\\{]@\n");
+	printf("Esperado: -;.]}]@\\{]@\n");*/
 	// base 16
-	printf("Bases 16, n = -65040. \"0123456789ABCDEF\" e \"jdlskmnMKZxVuzfa\"\n");
-	ft_putnbr_base(-65040, "0123456789ABCDEF");
+	printf("Bases 16, n = -65040. \"0123456789A2CDEF\" e \"jdlskmnMKZxVuzfa\"\n");
+	ft_putnbr_base(-650, "0123456789ABCDEF");
 	ft_putchar('\n');
-	printf("Esperado: -FE10\n");
+	/*printf("Esperado: -FE10\n");
 	ft_putnbr_base(-65040, "jdlskmnMKZxVuzfa");
 	ft_putchar('\n');
 	printf("Esperado: -afdj\n");
 	// base 0 e base 1, nao pode aparecer nada
 	printf("Bases 0 e 1, nao deve aparecer nada, n = 256.\n");
 	ft_putnbr_base(-29092, "0");
-	ft_putnbr_base(-29092, "");
+	ft_putnbr_base(-29092, "");*/
 	return (0);
-}*/
+}
--- c07/ex04/ft_convert_base.c
+++ c07/ex04/ft_convert_base.c
@@ -6,7 +6,123 @@
 /*   By: jrocha-v <jrocha-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 08:35:04 by jrocha-v          #+#    #+#             */
-/*   Updated: 2023/03/23 08:35:04 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2023/03/27 12:06:49 by jrocha-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include <stdio.h>
+
+int	ft_sl(char *base)
+{
+	int	i;
+
+	i = 0;
+	while (base[i] != '\0')
+		i++;
+	return (i);
+}
+
+int	ft_check_base(char *base)
+{
+	int	i;
+
+	i = -1;
+	while (base[++i] != '\0')
+	{
+		if (base[i] == '\0' || base[i] == '+'
+			|| base[i] == '-' || base[i] == base [i + 1]
+			|| ft_sl(base) <= 1)
+			return (0);
+		else
+			return (1);
+	}
+	return (0);
+}
+
+int	ft_pos(char *str, char *base, int i)
+{
+	int j;
+	int v;
+	
+	j = -1;
+	v = 0;
+	while (base[++j] != '\0')
+	{
+		if (str[i] == base[j])
+		{
+			v = j;
+			break;
+		}
+		else
+		v = -1;
+	}
+	return (v);
+}
+
+int	ft_rp(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	else if (power == 0)
+		return (1);
+	else
+		return (nb * (ft_rp(nb, (power - 1))));
+}
+
+char *ft_convert(int nb, char *base, int c)
+{
+	char	*l_out;
+	int		n;
+	int		j;
+	int		k;
+
+	l_out = malloc(34);
+	j = ft_sl(base);
+	if (nb < 0)
+	{
+		k = 0;
+		n = -nb;
+		c++;
+	}
+	else
+	n = nb;
+	if (n >= j)
+		ft_convert((n / j), base, ++c);
+	if (k == 0)
+		l_out[0] = '-';
+	l_out[++k] = base[n % j];
+	return (l_out);
+}
+
+char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
+{
+	int i;
+	int j;
+	int sign;
+	int n_conv1;
+	int c;
+	
+	i = 0;
+	c = 0;
+	sign = 1;
+	if (ft_check_base(base_from) == 0 || ft_check_base(base_to) == 0)
+	return (NULL);
+	while ((nbr[i] >= 9 && nbr[i] <= 13) || nbr[i] == 32)
+		i++;
+	while (nbr[i] == '+' || nbr [i] == '-')
+		if (nbr[i++] == '-')
+			sign = sign * -1;
+	j = -1;
+	while (++j < (ft_sl(nbr) - i))
+		n_conv1 = n_conv1 + ft_pos(nbr, base_from, (ft_sl(nbr) - 1 - j)) * ft_rp(ft_sl(base_from), j);
+	n_conv1 = sign * n_conv1;
+	return (ft_convert(n_conv1, base_to, c));
+}
+
+int	main(void)
+{
+	printf("%s", ft_convert_base("    +--256776564", "01234567", "0123456789abcdef"));
+	
+	return (0);
+}
