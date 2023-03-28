10e9a8f307b0361e60172cd03bcd37a3acf88ad0
Jose Guilherme Rocha Vaz Francisco <jrocha-v@c1r5s5.42porto.com>
2023-03-28 08:53:42 +0100

    c07

--- c07/ex05/ft_split.c
+++ c07/ex05/ft_split.c
@@ -6,7 +6,60 @@
 /*   By: jrocha-v <jrocha-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 08:35:24 by jrocha-v          #+#    #+#             */
-/*   Updated: 2023/03/23 08:35:25 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2023/03/27 18:59:55 by jrocha-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_count_words(char *str, char *charset)
+{
+	int i;
+	int j;
+	int c;
+	
+	i = 0;
+	j = 0;
+	c = 0;
+	while(str[i] != '\0')
+	{
+		while(charset[j] != '\0')
+		{
+			if (str[i] == charset[j])
+				c++;	
+			
+			
+		}
+	}
+}
+
+
+
+char	**ft_split(char *str, char *charset)
+{
+	int i;
+
+	i = 0;
+	i = ft_count_words(str, charset);
+
+
+
+}
+
+int		main(void)
+{
+	int		str_n;
+	char	*str;
+	char	*sep;
+	char	**strs;
+
+	str = "WfrONjnyoiSQ5GYKxJ6NSlqrUtPkklcoKR f";
+	sep = "ySzX";
+	strs = ft_split(str, sep);
+	str_n = 0;
+	while (strs[str_n] != 0)
+	{
+		printf("[%d]: %s\n", str_n, strs[str_n]);
+		str_n++;
+	}
+	free(strs);
+	return (0);
+}
