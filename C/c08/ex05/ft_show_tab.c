857afe15d9d98fb65a480c0bcd7494067f141042
José Francisco <jf@MacBook-Pro-de-Jose-2.local>
2023-04-10 18:24:21 +0100

    update

---
+++ C/c08/ex05/ft_show_tab.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_show_tab.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jf <jf@student.42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/10 16:38:12 by jf                #+#    #+#             */
+/*   Updated: 2023/04/10 16:54:07 by jf               ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_stock_str.h"
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+void	ft_putnbr(int nb)
+{
+	long	n;
+	
+	n = nb;
+	if (n < 0)
+	{
+		n = n * -1;
+		ft_putchar('-');
+	}
+	if (n >= 10)
+	{
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
+	}
+	else
+		ft_putchar(n + '0');
+}
+
+void	ft_show_tab(struct s_stock_str	*par)
+{
+	int	i;
+
+	i = 0;
+	while (par[i].str != 0)
+	{
+		ft_putstr(par[i].str);
+		ft_putchar('/n');
+		ft_putnbr(par[i].size);
+		ft_putchar('/n');
+		ft_putstr(par[i].copy);
+		ft_putchar('/n');
+		i++;		
+	}
+}
