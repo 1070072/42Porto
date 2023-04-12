fa8757a77550d3ff7de7f3ae4ebef47fadf4364d
Jose Guilherme Rocha Vaz Francisco <jrocha-v@c1r5s4.42porto.com>
2023-04-12 18:57:15 +0100

    lib

--- Common Core/Libft/functions/ft_strchr.c
+++ Common Core/Libft/functions/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: jrocha-v <jrocha-v@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 17:24:10 by jrocha-v          #+#    #+#             */
-/*   Updated: 2023/04/12 17:36:19 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2023/04/12 18:54:11 by jrocha-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,19 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 char	*ft_strchr(const char *str, int c)
 {
-	size_t	i;
+	int i;
 
 	i = 0;
-	if (str[i] == '\0')
+	if (*str == '\0')
 		return (0);
 	while (str[i])
 	{
-		if (str[i] == (char)c)
-			return (str[i]);
+		if (str[i] == c)
+			return ((char *)&str[i]);
 		i++;
 	}
 	return (0);
@@ -34,8 +35,7 @@ char	*ft_strchr(const char *str, int c)
 int	main(void)
 {
 	char *s1 = "Ola 42!";
-	int c = 'a';
 	
-	printf("%d", ft)
-	
+	printf("%s | %s\n", ft_strchr(s1, 'l'), strchr(s1, 'l'));
+	printf("%s | %s\n", ft_strchr(s1, 'a'), strchr(s1, 'a'));
 }
--- Common Core/Libft/functions/ft_strrchr.c
+++ Common Core/Libft/functions/ft_strrchr.c
@@ -6,24 +6,38 @@
 /*   By: jrocha-v <jrocha-v@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 17:33:18 by jrocha-v          #+#    #+#             */
-/*   Updated: 2023/04/12 17:36:24 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2023/04/12 18:55:33 by jrocha-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+//#include "libft.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+size_t	ft_strlen(const char *s);
 
 char	*ft_strrchr(const char *str, int c)
 {
-	size_t	i;
+	int i;
 
-	i = 0;
-	if (str[i] == '\0')
+	i = ft_strlen(str);
+	if (*str == '\0')
 		return (0);
-	while (str[i])
+	while (i > 0)
 	{
-		if (str[i] == (char)c)
-			return (str[i]);
-		i++;
+		if (str[i] == c)
+			return ((char *)&str[i]);
+		i--;
 	}
 	return (0);
+}
+
+int	main(void)
+{
+	char *s1 = "Ola 42 ola! l";
+	
+	printf("%s | %s\n", ft_strrchr(s1, 'l'), strrchr(s1, 'l'));
+	printf("%s | %s\n", ft_strrchr(s1, 'a'), strrchr(s1, 'a'));
 }
