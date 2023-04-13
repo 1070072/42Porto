564062738d09b6dd585c7e300537d4960ad1091e
Jose Guilherme Rocha Vaz Francisco <jrocha-v@c1r6s4.42porto.com>
2023-04-13 17:16:03 +0100

    lib
    ds

--- Common Core/Libft/functions/ft_strdup.c
+++ Common Core/Libft/functions/ft_strdup.c
@@ -6,16 +6,46 @@
 /*   By: jrocha-v <jrocha-v@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 16:50:10 by jrocha-v          #+#    #+#             */
-/*   Updated: 2023/04/13 16:50:54 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2023/04/13 17:15:35 by jrocha-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+size_t	ft_strlen(const char *s);
+
+char	*ft_strcpy(char *dest, const char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
 char	*ft_strdup(const char *str)
 {
+	char *temp;
 
+	temp = malloc(ft_strlen(str) + 1);
+	if (temp != NULL)
+	{
+		ft_strcpy(temp, str);
+		return (temp);
+	}
+	else
+		return (NULL);
+}
 
+int	main(void)
+{
+	char	str1[] = "Ola 42!";
 
-	
+	printf("Original: %s\n", str1);
+	printf("Duplicado: %s\n", ft_strdup(str1));
 }
