0f2de788db200aff1af5231437e77f5f97e9ed13
Jose Guilherme Rocha Vaz Francisco <jrocha-v@c1r4s3.42porto.com>
2023-04-14 15:30:07 +0100

    split

--- Common Core/Libft/functions/ft_split.c
+++ Common Core/Libft/functions/ft_split.c
@@ -6,28 +6,28 @@
 /*   By: jrocha-v <jrocha-v@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 09:01:33 by jrocha-v          #+#    #+#             */
-/*   Updated: 2023/04/14 12:00:55 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2023/04/14 15:29:55 by jrocha-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// Delete funtion for final version
+// Delete function for final version
 size_t	ft_strlen(const char *s)
 {
-	size_t	i;
-	
+	size_t    i;
+
 	i = 0;
 	while(s[i])
 		i++;
 	return (i);
 }
-///////////////////////
+////////////////////////////////////
 
 static int	ft_cntwrds(char const *s, char c)
 {
-	int i;
-	int words;
+    int	i;
+    int	words;
 
 	i = 0;
 	words = 0;
@@ -35,16 +35,16 @@ static int	ft_cntwrds(char const *s, char c)
 	{
 		if (s[i] != '\0' && (s[i + 1] == c || s[i + 1] == '\0'))
 			words++;
-		i++;		
+		i++;        
 	}
 	return (words);
 }
 
 static int	*ft_strposi(char const *s, char c, int words)
 {
-	size_t i;
-	int 	j;
-	int 	*posi; // Position of start and end of each string {s, e, s, e, ....}
+	size_t	i;
+	int		j;
+	int		*posi;
 
 	i = 0;
 	j = -1;
@@ -55,49 +55,59 @@ static int	*ft_strposi(char const *s, char c, int words)
 		while (s[i] != '\0' && s[i] != c)
 			i++;
 		posi[++j] = i - 1;
-		i++;		
+		i++;        
 	}
 	return (posi);
+}
+
+static char **ft_substrings(char const *s, int words, int *posi)
+{
+	char	**out;
+	int		i;
+	int		j;
+	int		k;
+	int		l;
+
+	i = -1;
+	j = 0;
+	l = -1;
+	out = malloc(sizeof(char *) * (words + 1));
+	while (++i < words)
+	{
+		k = -1;
+		out[i] = malloc(sizeof(char) * ((posi[j + 1] - posi[j]) + 2));
+		while (++k < ((posi[j + 1] - posi[j]) + 1))
+			out[i][k] = s[++l];
+		out[i][k] = '\0';
+		l++;
+		j = j + 2;
+	}
+	out[i] = '\0';
+	return (out);  
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**out;
-	int 	i;
-	int 	j;
-	int 	k;
-	int 	words;
+	int		words;
 	int		*posi;
-	
-	i = 0;
-	j = 0;
+
 	words = ft_cntwrds(s, c);
 	posi = ft_strposi(s, c, words);
+	out = ft_substrings(s, words, posi);
 	if (!s)
 		return (NULL);
-	out = malloc(sizeof(char *) * (words + 1));
-	while (i < words)
-	{
-		k = -1;
-		out[i] = malloc(sizeof(char) * ((posi[j + 1] - posi[j]) + 2));
-		while (++k < ((posi[j + 1] - posi[j]) + 1))
-			out[i][k] = s[j + k];
-		
-		j = j + 2;
-	}
-	return (out);	
+	return (out);
 }
 
 int main()
 {
-	char *s1 = "Ola 42 ola!";
-	char c = ' ';
-	int words = ft_cntwrds(s1, c);
-	int *posi = ft_strposi(s1, c, words);
-	int i = -1;
+    char *s1 = "Ola 42";
+    char c = ' ';
+    int i = -1;
+    char **out = ft_split(s1, c);
 
-	while (++i < (words * 2))
-		printf("%i ", posi[i]);
-
-	
+    i = -1;
+    while (++i < 2)
+        printf("%s\n", out[i]);
 }
