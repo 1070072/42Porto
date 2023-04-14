f047768dcdc0efef4dcdff5f2d5e6c74cf190d10
Jose Guilherme Rocha Vaz Francisco <jrocha-v@c1r4s3.42porto.com>
2023-04-14 12:01:42 +0100

    split

--- Common Core/Libft/functions/ft_split.c
+++ Common Core/Libft/functions/ft_split.c
@@ -6,12 +6,13 @@
 /*   By: jrocha-v <jrocha-v@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 09:01:33 by jrocha-v          #+#    #+#             */
-/*   Updated: 2023/04/14 11:40:17 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2023/04/14 12:00:55 by jrocha-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// Delete funtion for final version
 size_t	ft_strlen(const char *s)
 {
 	size_t	i;
@@ -21,6 +22,7 @@ size_t	ft_strlen(const char *s)
 		i++;
 	return (i);
 }
+///////////////////////
 
 static int	ft_cntwrds(char const *s, char c)
 {
@@ -38,11 +40,11 @@ static int	ft_cntwrds(char const *s, char c)
 	return (words);
 }
 
-static int	*ft_getstrlen(char const *s, char c, int words)
+static int	*ft_strposi(char const *s, char c, int words)
 {
 	size_t i;
 	int 	j;
-	int 	*posi; // Position of start and end of each string, consecutively
+	int 	*posi; // Position of start and end of each string {s, e, s, e, ....}
 
 	i = 0;
 	j = -1;
@@ -58,25 +60,44 @@ static int	*ft_getstrlen(char const *s, char c, int words)
 	return (posi);
 }
 
-/*char	**ft_split(char const *s, char c)
+char	**ft_split(char const *s, char c)
 {
 	char	**out;
+	int 	i;
+	int 	j;
+	int 	k;
+	int 	words;
+	int		*posi;
 	
+	i = 0;
+	j = 0;
+	words = ft_cntwrds(s, c);
+	posi = ft_strposi(s, c, words);
 	if (!s)
 		return (NULL);
-	out = malloc(sizeof(char *) * (ft_cntwrds(s, c) + 1));
-	
+	out = malloc(sizeof(char *) * (words + 1));
+	while (i < words)
+	{
+		k = -1;
+		out[i] = malloc(sizeof(char) * ((posi[j + 1] - posi[j]) + 2));
+		while (++k < ((posi[j + 1] - posi[j]) + 1))
+			out[i][k] = s[j + k];
+		
+		j = j + 2;
+	}
 	return (out);	
-}*/
+}
 
 int main()
 {
 	char *s1 = "Ola 42 ola!";
 	char c = ' ';
 	int words = ft_cntwrds(s1, c);
-	int *posi = ft_getstrlen(s1, c, words);
+	int *posi = ft_strposi(s1, c, words);
 	int i = -1;
 
 	while (++i < (words * 2))
 		printf("%i ", posi[i]);
+
+	
 }
